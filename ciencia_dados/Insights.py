e57196a8ebeef3d3127086520de2c204aca5f0d7
Gustavo H <88460364+kloin0@users.noreply.github.com>
2022-11-13 23:26:01 -0300

    Atualização do código
    link = https://medium.com/@meigarom/os-5-projetos-de-data-science-que-far%C3%A1-o-recrutador-olhar-para-voc%C3%AA-c32c67c17cc9

--- ciencia_dados/Insights.py
+++ ciencia_dados/Insights.py
@@ -2,47 +2,42 @@
 '''O objetivo do projeto de Insights
 é recomendar soluções para o negócio
 através de Insights gerados por uma ótima Análise Exploratória de Dados.'''
-from matplotlib import *
-import pandas as pd
-import matplotlib as plt 
-import zipfile 
-#Abrindo o arquivo .zip 
+import matplotlib.pyplot as plt 
+import pandas as pd 
+import seaborn as sn
+import numpy, scipy,statsmodels,zipfile
+
 dados = pd.read_csv('archive.zip',compression='zip')
-dados.rename(columns={'price':'Preço','bedrooms':'Quartos','bathrooms':'Banheiros','yr_renovated':'Ano renovado'},inplace=True)
 n = 10
-casas_mais_caras = dados.nlargest(n,'Preço')
-# Análisar o pq as casa são caras e identificar um padrão
-# Preço 
-def casas_preco():
-        # Sempre que for chamar a váriavel lambrar de chamar a função tbm 
-        global casa_barata
-        global casa_media
-        global casa_cara 
-        preco = dados['Preço']
-        casa_barata = preco.min()
-        casa_media = preco.mean()
-        casa_cara = preco.max()
-        # Pagando as 5 casas mais caras 
-dados_decresente = dados.sort_values(['Preço'],ascending=False)
-'''print(dados_decresente.head(100))
-print(dados.max())'''
-# Pegar as informações da casas limpo sem nenhum dado 
-def infomacao():
-        latitude = dados_decresente['lat']
-        longitude = dados_decresente['long']
-        numeros_quartos = dados_decresente['Quartos']
-        banheiros = dados_decresente['Banheiros']
-        ano_contruido = dados_decresente['yr_built']
-        renovado = dados_decresente['Ano renovado']
-        for i in dados_decresente:
-                informacao = i 
-                #print(informacao)
-        return casas_mais_caras
-# Agora é precisso pagar as 10 casas mais caras e ver o que elas tem em comum 
-def reconhecimento_padrao():
-        pass
-if __name__ == "__main__":
-        casas_preco()
-        infomacao()
-        comparacao()
+dados_altos = dados.nlargest(n,'price')
+dados_baixos = dados.sort_values(['price']).head(n)
+todos_dados = dados_baixos,dados_altos
+for k in range(n):
+	localizacao_alta = dados_altos[["long","lat"]]
+	planta_alto = dados_altos[["bedrooms","bathrooms"]]
+	estrutura_alto = dados_altos[["sqft_living","sqft_lot","sqft_basement","sqft_above"]] 
+	acabamento_alto = dados_altos[["yr_built","yr_renovated"]]
+	areas_de_lazer_alto = dados_altos[["view","waterfront"]]
+	seguranca_alto = dados_altos[["condition"]]
+	avaliacao_alto = dados_altos[["grade"]]		
+	infomacao_alta = localizacao_alta,planta_alto,estrutura_alto,acabamento_alto,areas_de_lazer_alto,seguranca_alto,avaliacao_alto
+preco = dados_altos['price']
+# Calculando a correlação do df 
+p = dados_altos.corr()
+print(p)
+# COdigo para ver se o correlacao e positiva negativa ou inesistente 
+if ps == 0.9 and 1 or -0.9 and-1:
+	print('Correlação muito forte')
+elif p == 0.7 and 0.9 or -0.7 and -0.9:
+	print('Correlação forte')
+elif p == 0.5 and 0.7 or -0.5 and -0.7:
+	print('Correlação moderada')
+elif p == 0.3 and 0.5 or -0.3 and -0.5:
+	print('Correlação fraca')
+else:
+	print('Não a Correlação')
+# nome das colunas
+for c in dados:
+	pass 
+
                                                Projeto em andamento 
