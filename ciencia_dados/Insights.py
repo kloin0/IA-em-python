b608573c510b8bbbcd5f21052b1975bbe62ced5d
Gustavo H <88460364+kloin0@users.noreply.github.com>
2022-11-18 19:22:34 -0300

    Código 2.0
    link = https://medium.com/@meigarom/os-5-projetos-de-data-science-que-far%C3%A1-o-recrutador-olhar-para-voc%C3%AA-c32c67c17cc9

--- ciencia_dados/Insights.py
+++ ciencia_dados/Insights.py
@@ -3,41 +3,25 @@
 é recomendar soluções para o negócio
 através de Insights gerados por uma ótima Análise Exploratória de Dados.'''
 import matplotlib.pyplot as plt 
-import pandas as pd 
-import seaborn as sn
+import pandas as pd  
+import seaborn as sns
 import numpy, scipy,statsmodels,zipfile
 
 dados = pd.read_csv('archive.zip',compression='zip')
-n = 10
-dados_altos = dados.nlargest(n,'price')
-dados_baixos = dados.sort_values(['price']).head(n)
-todos_dados = dados_baixos,dados_altos
-for k in range(n):
-	localizacao_alta = dados_altos[["long","lat"]]
-	planta_alto = dados_altos[["bedrooms","bathrooms"]]
-	estrutura_alto = dados_altos[["sqft_living","sqft_lot","sqft_basement","sqft_above"]] 
-	acabamento_alto = dados_altos[["yr_built","yr_renovated"]]
-	areas_de_lazer_alto = dados_altos[["view","waterfront"]]
-	seguranca_alto = dados_altos[["condition"]]
-	avaliacao_alto = dados_altos[["grade"]]		
-	infomacao_alta = localizacao_alta,planta_alto,estrutura_alto,acabamento_alto,areas_de_lazer_alto,seguranca_alto,avaliacao_alto
-preco = dados_altos['price']
-# Calculando a correlação do df 
-p = dados_altos.corr()
-print(p)
-# COdigo para ver se o correlacao e positiva negativa ou inesistente 
-if ps == 0.9 and 1 or -0.9 and-1:
-	print('Correlação muito forte')
-elif p == 0.7 and 0.9 or -0.7 and -0.9:
-	print('Correlação forte')
-elif p == 0.5 and 0.7 or -0.5 and -0.7:
-	print('Correlação moderada')
-elif p == 0.3 and 0.5 or -0.3 and -0.5:
-	print('Correlação fraca')
-else:
-	print('Não a Correlação')
-# nome das colunas
-for c in dados:
-	pass 
-
-                                               Projeto em andamento 
+n = 10 
+dados['id'].nunique()
+casa_mais_cara = dados.nlargest(n,'price')
+tamanho_casa = casa_mais_cara['sqft_living'],casa_mais_cara['sqft_above'],casa_mais_cara['sqft_living15']
+tamanho_terreno = casa_mais_cara['sqft_lot'],casa_mais_cara['sqft_lot15']
+lazer_casa = casa_mais_cara['waterfront'],casa_mais_cara['view']
+localizacao_casa = casa_mais_cara['lat'],casa_mais_cara['long']
+seguranca_casa = casa_mais_cara['condition'],casa_mais_cara['yr_renovated']
+def media(media_casas_caras):
+        media_quatos = casa_mais_cara.groupby('price').bedrooms.mean()
+        media_banhiros = casa_mais_cara.groupby('price').bathrooms.mean()
+        media_tamanho = casa_mais_cara.groupby('price').tamanho_terreno.mean()  
+        media_lazer = casa_mais_cara.groupby('price').lazer_casa.mean()
+        media_localizacao = casa_mais_cara.groupby('price').localizacao_casa.mean()
+        media_seguranca = casa_mais_cara.groupby('price').seguranca_casa.mean()
+        media_casas_caras = media_seguranca,media_localizacao,media_lazer,media_banhiros,media_quatos
+~
